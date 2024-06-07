916bb2e64a19eb5e5211a5e900b6141db7d5f21e
Dimple Dhawan <dimple.dhawan@gmail.com>
2024-06-07 01:03:05 -0400

    Fix ability to post new user

--- hypnoinsight-api/controllers/userController.js
+++ hypnoinsight-api/controllers/userController.js
@@ -1,11 +1,7 @@
 const knex = require('knex')(require('../knexfile'));
 
 const addUser = async (req, res) => {
-    if (!req.body.user_id) {
-        return res.status(400).json({
-            message: `Unable to save user.`
-        });
-    } else if (!req.body.first_name) {
+    if (!req.body.first_name) {
         return res.status(400).json({
             message: `First name is required.`
         });
@@ -18,22 +14,24 @@ const addUser = async (req, res) => {
             message: `Email is required.`
         });
     }
-    
+
     try {
-        const user_interests = await knex('user_interests').insert({
-            user_id: req.body.user_id,
-            interest_id: req.body.interest_id,
+        const users = await knex('users').insert({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email
         });
 
-        const newInterestId = user_interests[0];
-        const newInterest = await knex('user_interests')
-            .where({ id: newInterestId })
-            .first();
-        res.status(201).json(newInterest);
+        const newUserId = users[0];
+
+        const newUser = await knex('users')
+        .where({id: newUserId})
+        .first()
+            res.status(201).json(newUser);
 
     } catch (error) {
         res.status(500).json({
-            message: `Unable to save new user interest(s): ${error}`
+            message: `Unable to save new user: ${error}`
         });
     }
 }
--- hypnoinsight-api/routes/userRoute.js
+++ hypnoinsight-api/routes/userRoute.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/userController");
 router
     .route("/")
     .post(userController.addUser);
+    
 
 module.exports = router;
