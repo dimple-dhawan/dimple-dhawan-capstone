f491aec667c4226fffdf1bf0ed138f5fdd57a395
Dimple Dhawan <dimple.dhawan@gmail.com>
2024-06-08 02:32:38 -0400

    Edit completion text

--- hypnoinsight/src/components/MilestoneNuggets/MilestoneNuggets.jsx
+++ hypnoinsight/src/components/MilestoneNuggets/MilestoneNuggets.jsx
@@ -6,13 +6,10 @@ function MilestoneNuggets ({factsCompleted}) {
             <div className="milestone__facts">
                 <h2>Congratulations!</h2>
                 <div className="milestone__completed-text"> 
-                    You have completed the Hypnosis 'Did You "Know' module and that concludes this mini-course on Understanding Hypnosis.
+                    You have completed the mini-course on Understanding Hypnosis.
                 </div>
                 <div className="milestone__continue-text"> 
-                    If you are ready and willing for change now through hypnosis to achieve your goals, please setup a consultation with me or send me an <a className="email" href="mailto:jassteena@empowermentVoyage.com">email</a> and mention the code '<span className="completion-code">READYNOW2024</span>'.
-                </div>
-                <div className="milestone__next-text">
-                    
+                    If you are ready for change now to achieve your goals, please setup a consultation with me or send me an <a className="email" href="mailto:jassteena@empowermentVoyage.com">email</a> and mention the code '<span className="completion-code">READYNOW2024</span>'.
                 </div>
             </div>
         </section>
