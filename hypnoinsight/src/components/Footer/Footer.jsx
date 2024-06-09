d6d977fb97aead61cae3af56f685a4f3389cada1
Dimple Dhawan <dimple.dhawan@gmail.com>
2024-06-08 23:19:44 -0400

    add text and book now to footer

--- hypnoinsight/src/components/Footer/Footer.jsx
+++ hypnoinsight/src/components/Footer/Footer.jsx
@@ -1,9 +1,21 @@
 import "./Footer.scss";
+import {Link} from 'react-router-dom';
 
 function Footer() {
     return (
         <footer className="footer">
-            Copyright &copy;2024 Empowerment Voyage
+            <div className="footer__tag">
+            Start your journey towards a happier, healthier, and more fulfilling life.
+            </div>
+            <div className="footer__booknow">
+                <Link to="/booknow">
+                    BOOK NOW
+                </Link>
+            </div>
+            <div>
+                Copyright &copy;2024 Empowerment Voyage
+            </div>
+            
         </footer>
     )
 }
