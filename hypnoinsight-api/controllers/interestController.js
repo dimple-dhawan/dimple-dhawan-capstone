5cc013f3b23f8c155432c5de839535474a313303
Dimple Dhawan <dimple.dhawan@gmail.com>
2024-06-09 19:55:59 -0400

    only returning id and interest from interests table to get request

--- hypnoinsight-api/controllers/interestController.js
+++ hypnoinsight-api/controllers/interestController.js
@@ -2,7 +2,7 @@ const knex = require("knex")(require("../knexfile"));
 
 const interests = async (_req, res) => {
     try {
-        const data = await knex("interests");
+        const data = await knex("interests").select({id: 'id', interest: 'interest'});
         res.status(200).json(data);
     } catch (error) {
         res.status(500).send(`Error retrieving interests: ${error}`);
