aa3d2a9b554cf93b7952d4b64ecbdf523aaabb92
Dimple Dhawan <dimple.dhawan@gmail.com>
2024-06-10 00:17:26 -0400

    fixed desktop prelude page on module

--- hypnoinsight/src/components/Prelude/Prelude.jsx
+++ hypnoinsight/src/components/Prelude/Prelude.jsx
@@ -9,13 +9,13 @@ function Prelude({setStartLearning}) {
         <section className="welcome-section">
             <h2>Welcome</h2>
 
-            <div className="welcome">
+            <div className="welcome-module">
                 After completing this mini-course, you will feel more confident, having the basic understanding of hypnosis and what's to come after you book a hypnosis session with me.
             </div>
             <div className="welcome">
             The content is divided into easily digestible portions across three categories: facts, myths, and 'Did You Know' 
             </div>
-            <div className="welcome">
+            <div className="welcome-module">
                 Enjoy!
             </div>
             <div className="btn">
